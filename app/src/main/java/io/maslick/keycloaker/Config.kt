b45b233a26432e42b5cf74a6d0d5404ea1a3c150
Ming Li <ftntming@gmail.com>
2021-01-17 18:17:16 -0800

    update config

--- app/src/main/java/io/maslick/keycloaker/Config.kt
+++ app/src/main/java/io/maslick/keycloaker/Config.kt
@@ -2,7 +2,7 @@ package io.maslick.keycloaker
 
 object Config {
     const val clientId = "barkoder-frontend"
-    const val baseUrl = "https://activeclouder.ijs.si/auth/realms/barkoder/protocol/openid-connect"
+    const val baseUrl = "https://sso.fortivoice-cloud.net/auth/realms/chat.vanaaa.ca/protocol/openid-connect"
     const val authenticationCodeUrl = "$baseUrl/auth"
     const val redirectUri = "maslick://oauthresponse"
 }
